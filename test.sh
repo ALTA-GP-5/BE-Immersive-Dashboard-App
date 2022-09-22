e02fae792bf8a6d36ea6a0f445335ab103709845
muhdjau <mhmd.jauhari@gmail.com>
2022-09-22 15:03:16 +0700

    (feat) mentor: add unit testing

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 source ./.env
 go test ./domains/login/... -coverprofile=cover.out && go tool cover -html=cover.out
+go test ./domains/mentor/... -coverprofile=cover.out && go tool cover -html=cover.out
